fecdaad71959994a26d857b81d2ca124433a678e
markimagemTv <markimagem2014@gmail.com>
2025-05-05 23:43:17 -0300

    Update bot_completo.py

--- bot_completo.py
+++ bot_completo.py
@@ -97,6 +97,7 @@ PRODUCT_CATALOG = {
         {"name": "☁️ CLOUDDY R$65", "price": 65.00, "fields": ["E-mail", "Senha do app"]},
     ],
     "COMPRAR CRÉDITOS": [
+        {"name": "🎯 X SERVER PLAY (13,50und)", "price": 13.50, "discount": True},
         {"name": "⚡ FAST PLAYER (13,50und)", "price": 13.50, "discount": True},
         {"name": "👑 GOLD PLAY (13,50und)", "price": 13.50, "discount": True},
         {"name": "📺 EI TV (13,50und)", "price": 13.50, "discount": True},
