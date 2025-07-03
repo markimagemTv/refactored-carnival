325fd7550e9ed575247d05599cfbbf1f15ba8580
markimagemTv <markimagem2014@gmail.com>
2025-07-03 11:24:23 -0300

    Update bot_completo.py

--- bot_completo.py
+++ bot_completo.py
@@ -86,7 +86,7 @@ PRODUCT_CATALOG = {
         {"name": "➕​ ASSIST+ R$ 65", "price": 65.00, "fields": ["MAC"]},
         {"name": "📱 NINJA PLAYER R$65", "price": 65.00, "fields": ["MAC", "CHAVE OTP"]},
         {"name": "📺 MEGA IPTV R$ 75", "price": 75.00, "fields": ["MAC"]},
-        {"name": "🧠 SMART ONE R$60", "price": 60.00, "fields": ["MAC"]},
+        {"name": "🧠 SMART ONE R$60", "price": 70.00, "fields": ["MAC"]},
         {"name": "🎮 IBO PRO PLAYER R$50", "price": 50.00, "fields": ["MAC"]},
         {"name": "📡 IBO TV OFICIAL R$50", "price": 50.00, "fields": ["MAC"]},
         {"name": "🧩 DUPLECAST R$60", "price": 60.00, "fields": ["MAC", "CHAVE OTP"]},
